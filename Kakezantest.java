512559e0db032d8514567289e0f58c73d275490b
井口広也 <iguchi.hiroya@m.alhinc.jp>
2017-09-08 17:26:22 +0900

    九九のテスト

---
+++ Kakezantest.java
@@ -0,0 +1,16 @@
+package jp.alhinc.iguchi_hiroya.calculate_sales;
+
+public class Kakezantest {
+	public static void main(String[] args){
+
+		System.out.println("九九");
+		for(int i= 1; i<=9;i++){
+			for(int j=1; j<=9;j++){
+				int sum = i*j;
+			System.out.print(sum+" ");
+		}
+			System.out.println("");
+	}
+
+	}
+}
